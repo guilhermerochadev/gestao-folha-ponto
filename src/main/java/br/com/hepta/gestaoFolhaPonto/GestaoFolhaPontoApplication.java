447a78047b9d3d96c92c81c6e7797967f6178a24
Guilherme Barbosa Sales da Rocha <guilherme.rocha@hepta.com.br>
2023-09-26 17:42:07 -0300

    Criação do projeto

---
+++ src/main/java/br/com/hepta/gestaoFolhaPonto/GestaoFolhaPontoApplication.java
@@ -0,0 +1,13 @@
+package br.com.hepta.gestaoFolhaPonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFolhaPontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFolhaPontoApplication.class, args);
+	}
+
+}
